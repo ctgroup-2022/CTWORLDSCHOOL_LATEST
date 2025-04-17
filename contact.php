c7f708ae4b9f698e369a1b463de72d7835e03a52
kashish <singhkashish364@.com>
2025-04-17 10:38:26 +0530

    heading tag change

--- contact.php
+++ contact.php
@@ -80,7 +80,7 @@
    <!-- page header starts -->
    <div class="container-fluid bg-breadcrumb">
         <div class="container text-center py-5" style="max-width: 900px;">
-            <h4 class="text-white display-4 mb-5 wow fadeInDown" data-wow-delay="0.1s">Contact Us</h4>
+            <h1 class="text-white display-4 mb-5 wow fadeInDown" data-wow-delay="0.1s">Contact Us</h1>
             <ol class="breadcrumb d-flex justify-content-center mb-0 wow fadeInDown" data-wow-delay="0.3s">
                 
             </ol>    
